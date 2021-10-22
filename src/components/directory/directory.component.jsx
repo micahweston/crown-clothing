17e95b6fb97b66fc4fa9ba74c301ab538133194c
Micah Weston <micah.weston@gmail.com>
2021-10-22 11:59:03 -0500

    added our shop components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,12 +45,12 @@ class Directory extends React.Component {
       ],
     };
   }
-
+  // We are spreading out our key value pairs from our object all but the id. in otherSectionProps
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={ id } title={ title } imageUrl={ imageUrl }  size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={ id } {...otherSectionProps} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from "react";
+// Higher order component. Takes a component and transforms it into another component to send.
+import { withRouter } from "react-router-dom";
 
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div 
-            className="background-image" 
-            style={{backgroundImage: `url(${imageUrl})`}}
-        />
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
+// Using linkURL and match.url we are finding the URL we clicked on, and finding it from directory.component.
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
+  </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
